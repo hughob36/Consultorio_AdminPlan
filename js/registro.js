9a1c5257c9bbee2162046f25891452f9b56707a9
Hugo Benitez <hugo.boca16@gmail.com>
2026-04-08 18:20:12 -0300

    registrar y mandar a index

--- js/registro.js
+++ js/registro.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             if (response.ok) {
                 alert("¡Usuario registrado con éxito!");
-                window.location.href = 'login.html'; 
+                window.location.href = 'index.html'; 
             } else {
                 // Si el error es 403 o 401, el token probablemente venció
                 if(response.status === 403 || response.status === 401) {
